0617e35a3915dca68b3b963993cfcfb6eb6a0f6c
TheMisterSenpai <61903983+TheMisterSenpai@users.noreply.github.com>
2021-04-17 19:34:25 +0300

    Почему?

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -11,14 +11,17 @@ class errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        emb = discord.Embed(title = ':warning: О нет, произошла ошибка в команде!', color=discord.Color.red())
         if isinstance(error, commands.BadArgument):
+            emb = discord.Embed(title = ':warning: О нет, произошла ошибка в команде!', color=discord.Color.red())
             emb.add_field(name = 'Ошибка:', value = f'> Пользователь не был найден на сервере или в базе данных' )
         if isinstance(error, commands.errors.MissingPermissions):
+            emb = discord.Embed(title = ':warning: О нет, произошла ошибка в команде!', color=discord.Color.red())
             emb.add_field(name = 'Ошибка:', value = f'> У вас не хватает правил для использование этой команды' )
         if isinstance(error, commands.errors.CommandInvokeError):
+            emb = discord.Embed(title = ':warning: О нет, произошла ошибка в команде!', color=discord.Color.red())
             emb.add_field(name = 'Ошибка:', value = f'> Возможно у меня не хватает правил или ошибка на стороне кода' )
         if isinstance(error, commands.errors.MissingRequiredArgument):
+            emb = discord.Embed(title = ':warning: О нет, произошла ошибка в команде!', color=discord.Color.red())
             emb.add_field(name = 'Ошибка:', value = f'> Не правильное использование {ctx.message.content}! Пожалуйста проверьте через `mhelp <команда>`' )              
         await ctx.send(embed = emb)    
 
